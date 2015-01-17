eeada4fc1717f99b6c4611f47871ecf7573cbba2
Alec Iverson <adiverso93@gmail.com>
2015-01-17 12:18:31 -0700

    IT'S ALIVE!!!!! It works. Woo.
    Re-did constructor queue adding. A bit more robust now as well.
    Changed Team building list to use key-value pairs, rather than index-value pairs. This helps keep 1 building per path as well.
    Re-did Team:addObject(h) as well. Works now. Woot.

--- aiBuild.lua
+++ aiBuild.lua
@@ -93,13 +93,12 @@ aiBuild.Constructor =
 function aiBuild.Constructor:update()
 	--sort the queue by priority
 	table.sort(self.queue, function(one, two) return one.priority > two.priority end)
-
+	
 	if IsValid(self.handle) then
 		if #self.queue == 0 then
 			Goto(self.handle, GetRecyclerHandle(self.team), 0)
-		elseif CanCommand(self.handle) and CanBuild(self.handle) and not IsBusy(self.handle) then
+		elseif CanBuild(self.handle) and not IsBusy(self.handle) then
 			BuildAt(self.handle, self.queue[1].odf, self.queue[1].path)
-			table.remove(self.queue, 1)
 		end
 		
 		return true		--all is well in the world. Minus the battle going on
@@ -147,18 +146,23 @@ function aiBuild.Team:update()
 	end
 	
 	--iterate over buildings, if  destroyed, then add to queue to build
-	for i, b in ipairs(self.buildingList) do
-		if b.handle == nil or not IsValid(b.handle) then
-			local inQueue = false
-			for j, v in ipairs(self.constructor.queue) do
-				if v == b then
-					inQueue = true
-					break
+	for p, b in pairs(self.buildingList) do
+		if not IsValid(b.handle) then
+			local h = GetNearestObject(p)
+			if IsOdf(h, b.odf) and GetDistance(h, p) < 60 then
+				b.handle = h
+			else
+				local inQueue = false
+				for i, v in ipairs(self.constructor.queue) do
+					if v.path == p then
+						inQueue = true
+						break
+					end
 				end
-			end
 			
-			if not inQueue then
-				table.insert(self.constructor.queue, b)
+				if not inQueue then
+					table.insert(self.constructor.queue, b)
+				end
 			end
 		end
 	end
@@ -173,17 +177,10 @@ function aiBuild.Team:addObject(h)
 	
 	if IsBuilding(h) then
 		--if the building is the right type, and it's basically at the correct path, it's the right building
-		--[[if IsOdf(h, self.constructor.queue[1].odf) and GetDistance(h, self.constructor.queue[1].path) < 60 then
-			self.constructor.queue[1].handle = h
-			
-			for i, b in ipairs(self.buildingList) do
-				if self.constructor.queue[1] == b then
-					b.handle = h
-				end
-			end
-			
+		if IsOdf(h, self.constructor.queue[1].odf) and IsWithin(h, self.constructor.handle, 60) then
+			self.buildingList[self.constructor.queue[1].path].handle = h
 			table.remove(self.constructor.queue, 1)
-		end]]
+		end
 	elseif IsOdf(h, aiBuild.Faction[self.faction].constructor) then	--got a new constructor.
 		self.constructor.handle = h
 		self.makingNewConst = false
@@ -196,7 +193,7 @@ function aiBuild.Team:addBuilding(odf, path, priority)
 	newBuilding.odf = odf
 	newBuilding.path = path
 	newBuilding.priority = priority
-	table.insert(self.buildingList, newBuilding)
+	self.buildingList[path] = newBuilding
 end
 
 aiBuild.Team.__index = aiBuild.Team
